a6c990f922819b6fb424dae3518cb72d92527fe2
imeshsaminda <115490529+imeshsaminda@users.noreply.github.com>
2025-02-11 09:08:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'session id', //add your session id
+SESSION_ID: process.env.SESSION_ID || 'SUPUN-MD=3Y1hlTya#zPjwgej-1l01KgLVtsIfXVjjtZ8ZfXl9uiB07Oth3z4', //add your session id
 MONGODB: process.env.MONGODB || "mongodb url", //add your mongodb url
  URL : process.env.BOT_MENU_LINKS || 'https://i.ibb.co/bHXBV08/9242c844b83f7bf9.jpg',
 MAX_SIZE: 500
